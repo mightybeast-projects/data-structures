66808c2eef3fcbe65e1fa601012f67a64b3a0028
MightyBeast <zgpoter@gmail.com>
2023-05-26 00:24:10 +0300

    -feat- : Bubble sort.

---
+++ Algorithms/BubbleSort/BubbleSort.test.ts
@@ -0,0 +1,7 @@
+import bubbleSort from "./BubbleSort";
+
+test("bubble sort sorts array", () => {
+    const arr = [1, 8, 3, 7, 4, 2];
+    bubbleSort(arr);
+    expect(arr).toEqual([1, 2, 3, 4, 7, 8]);
+});
