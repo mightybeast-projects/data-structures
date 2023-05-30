496d0099b8337495575aaa57ca8c561d64468066
MightyBeast <zgpoter@gmail.com>
2023-05-30 16:13:17 +0300

    -feat- : BTBFS.

---
+++ Algorithms/BTBFS/BTBFS.test.ts
@@ -0,0 +1,50 @@
+import bfs from "./BTBFS";
+import BinaryNode from "../../DataStructures/BinaryTree/BinaryTreeOrder";
+
+const tree: BinaryNode<number> = {
+    value: 20,
+    right: {
+        value: 50,
+        right: {
+            value: 100,
+            right: null,
+            left: null,
+        },
+        left: {
+            value: 30,
+            right: {
+                value: 45,
+                right: null,
+                left: null,
+            },
+            left: {
+                value: 29,
+                right: null,
+                left: null,
+            }
+        },
+    },
+    left: {
+        value: 10,
+        right: {
+            value: 15,
+            right: null,
+            left: null,
+        },
+        left: {
+            value: 5,
+            right: {
+                value: 7,
+                right: null,
+                left: null,
+            },
+            left: null,
+        }
+    }
+};
+
+test("bt bfs test", () => {
+    expect(bfs(tree, 45)).toEqual(true);
+    expect(bfs(tree, 7)).toEqual(true);
+    expect(bfs(tree, 69)).toEqual(false);
+});
---
+++ Algorithms/BTBFS/BTBFS.ts
@@ -0,0 +1,23 @@
+import BinaryNode from "../../DataStructures/BinaryTree/BinaryTreeOrder";
+import Queue from "../../DataStructures/Queue/Queue";
+
+export default function bfs<T>(tree: BinaryNode<T>, value: T): boolean {
+    const queue = new Queue<BinaryNode<T> | null>();
+
+    queue.enqueue(tree);
+
+    while (queue.length) {
+        const node = queue.deque() as BinaryNode<T>;
+        
+        if (!node)
+            continue;
+
+        if (node.value === value)
+            return true;
+
+        queue.enqueue(node.left);
+        queue.enqueue(node.right);
+    }
+
+    return false;
+}
