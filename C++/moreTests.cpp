5385a8c3eef9aa172c9d31995abe232b79010959
MightyBeast <zgpoter@gmail.com>
2023-11-01 16:01:32 +0200

    -feat- : Adding test source example.

---
+++ C++/moreTests.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(MoreTests, BasicAssertions)
+{
+    EXPECT_EQ(7 * 6, 42);
+}
--- C++/mytests.cpp
+++ C++/mytests.cpp
@@ -1,13 +1,15 @@
 #include <gtest/gtest.h>
 #include "myfunctions.h"
 
-TEST(myfunctions, add)
+TEST(MyTests, BasicAssertions)
 {
-    GTEST_ASSERT_EQ(add(10, 22), 32);
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
 }
 
-int main(int argc, char *argv[])
+TEST(MyTests, AddTest)
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    EXPECT_EQ(add(10, 10), 20);
 }
