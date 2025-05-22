2e496cf3e1080fac38e874e6ed8ce770b7f75456
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-05-22 16:18:49 +0300

    -refactor- : Ignore.

--- CSharp/Algorithms/BinarySearch/BinarySearch.Tests.cs
+++ CSharp/Algorithms/BinarySearch/BinarySearch.Tests.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using NUnit.Framework;
 
 namespace CSharp.Algorithms.BinarySearch;
@@ -8,7 +7,8 @@ public class BinarySearchTests
 {
     [Test]
     [TestCase((int[])[1], 1)]
-    [TestCase((int[])[1, 2, 3, 4], 3)]
+    [TestCase((int[])[1, 2], 2)]
+    [TestCase((int[])[1, 12, 15, 25, 45, 90], 25)]
     public static void Assert_Item_Is_Found(int[] arr, int n)
     {
         Assert.That(BinarySearch.Search(arr, n), Is.True);
--- CSharp/Algorithms/LinearSearch/LinearSearch.Tests.cs
+++ CSharp/Algorithms/LinearSearch/LinearSearch.Tests.cs
@@ -5,25 +5,21 @@ namespace CSharp.Algorithms.LinearSearch;
 [TestFixture]
 public class LinearSearchTests
 {
-    private static readonly object[] trueCases =
-    [
-        new object[] { new int[] { 0 }, 0 },
-        new object[] { new int[] { 0, 1 }, 1 },
-        new object[] { new int[] { 4, 564, 2, 12, 67, 3, 75, 223 }, 75 },
-    ];
-
-    private static readonly object[] falseCases =
-    [
-        new object[] { new int[] { 0 }, -1 },
-        new object[] { new int[] { 0 }, 1 },
-        new object[] { new int[] { 4, 564, 2, 12, 67, 3, 75, 223 }, 76 },
-    ];
-
-    [TestCaseSource(nameof(trueCases))]
-    public void Assert_Item_Is_Found(int[] arr, int n) =>
+    [Test]
+    [TestCase((int[])[0], 0)]
+    [TestCase((int[])[0, 1], 1)]
+    [TestCase((int[])[4, 564, 2, 12, 67, 3, 75, 223], 75)]
+    public void Assert_Item_Is_Found(int[] arr, int n)
+    {
         Assert.That(LinearSearch.Search(arr, n), Is.True);
+    }
 
-    [TestCaseSource(nameof(falseCases))]
-    public void Assert_Item_Not_Found(int[] arr, int n) =>
+    [Test]
+    [TestCase((int[])[0], -1)]
+    [TestCase((int[])[0, 1], 2)]
+    [TestCase((int[])[4, 564, 2, 12, 67, 3, 75, 223], 76)]
+    public void Assert_Item_Not_Found(int[] arr, int n)
+    {
         Assert.That(LinearSearch.Search(arr, n), Is.False);
+    }
 }
