e0757300dfbf24e4c5359a9b222877a3b33b7173
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-05-23 14:31:13 +0300

    -chore- : Ignore.

--- CSharp/Algorithms/BubbleSort/BubbleSort.Tests.cs
+++ CSharp/Algorithms/BubbleSort/BubbleSort.Tests.cs
@@ -12,11 +12,11 @@ public class BubbleSortTests
     }
 
     [Test]
-    public void Assert_Array_Is_Sorted()
+    [TestCase((int[])[1], (int[])[1])]
+    [TestCase((int[])[1, 3, 2], (int[])[1, 2, 3])]
+    [TestCase((int[])[9, 4, 1, 6, 3, 2], (int[])[1, 2, 3, 4, 6, 9])]
+    public void Assert_Array_Is_Sorted(int[] arr, int[] expected)
     {
-        int[] arr = [9, 4, 1, 6, 3, 2];
-        int[] expected = [1, 2, 3, 4, 6, 9];
-
         Assert.That(BubbleSort.Sort(arr), Is.EqualTo(expected));
     }
 }
