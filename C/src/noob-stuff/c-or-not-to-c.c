e40728288f3fb6383f481b46f101e47210e6e66e
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-07-11 14:37:15 +0300

    -feat- : To c or not to c.

--- C/src/noob-stuff/c-or-not-to-c.c
+++ C/src/noob-stuff/c-or-not-to-c.c
@@ -1,24 +1,23 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+void toCOrNotToC()
+{
+    printf("To c or not to c; \n");
+    printf("That is the question. \n");
+
+    bool toC = true;
+    bool notToC = false;
+
+    if (toC || notToC)
+        printf("Just c already. \n");
+
+    return;
+}
+
 int main()
 {
     toCOrNotToC();
 
     return 0;
-}
-
-void toCOrNotToC()
-{
-    printf("to c or not to c \n");
-    printf("that is the question. \n");
-
-    bool toC = true;
-    bool notToC = false;
-
-    if (toC)
-        printf("just c already \n");
-
-    if (notToC)
-        printf("nah man, do not c \n");
 }
