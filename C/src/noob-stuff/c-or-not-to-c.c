d7ee7911f39b0c58cc65aa5fee0eef4846dedc9a
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-07-11 14:34:58 +0300

    -chore- : /home/yevgenij.yefremov@feofaniya.lan/Projects/data-structures/C/build/tests

--- C/src/noob-stuff/c-or-not-to-c.c
+++ C/src/noob-stuff/c-or-not-to-c.c
@@ -1,8 +1,24 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int main()
 {
-    printf("to c or not to c \n");
+    toCOrNotToC();
 
     return 0;
+}
+
+void toCOrNotToC()
+{
+    printf("to c or not to c \n");
+    printf("that is the question. \n");
+
+    bool toC = true;
+    bool notToC = false;
+
+    if (toC)
+        printf("just c already \n");
+
+    if (notToC)
+        printf("nah man, do not c \n");
 }
