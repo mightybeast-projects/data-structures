487b17af4b07411b39b68070750ca4da08e10d95
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-07-14 12:27:53 +0300

    -feat- : UPC check digit.

--- C/src/noob-stuff/main.c
+++ C/src/noob-stuff/main.c
@@ -53,14 +53,35 @@ void toCelcius()
     printf("Temperature in Celsius: %.1f\n", inFahrenheit - FAHRENHEIT_OFFSET);
 }
 
-int main()
+void upcCheckDigit()
+{
+    int d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11;
+
+    printf("Enter type (first) digit: ");
+    scanf("%1d", &d1);
+
+    printf("Enter first group of upc digits: ");
+    scanf("%1d%1d%1d%1d%1d", &d2, &d3, &d4, &d5, &d6);
+
+    printf("Enter second group of upc digits: ");
+    scanf("%1d%1d%1d%1d%1d", &d7, &d8, &d9, &d10, &d11);
+
+    int firstGroupSum = d1 + d3 + d5 + d7 + d9 + d11;
+    int secondGroupSum = d2 + d4 + d6 + d8 + d10;
+    int totalSum = firstGroupSum * 3 + secondGroupSum - 1;
+    int dCheck = 9 - totalSum % 10;
+
+    printf("Your check digit: %d\n", dCheck);
+}
+
+void main()
 {
     // toCOrNotToC();
     // largestNumbers();
     // shippingBoxes();
-    toCelcius();
+    // toCelcius();
 
-    return 0;
+    upcCheckDigit();
 }
 
 // DO NOT USE COMMENTS
