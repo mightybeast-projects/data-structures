9f0e8e492b8675da90900743995985048e007859
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-07-14 12:53:08 +0300

    -refactor- : UPC code refactoring.

--- C/src/noob-stuff/main.c
+++ C/src/noob-stuff/main.c
@@ -57,14 +57,19 @@ void upcCheckDigit()
 {
     int d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11;
 
-    printf("Enter type (first) digit: ");
-    scanf("%1d", &d1);
-
-    printf("Enter first group of upc digits: ");
-    scanf("%1d%1d%1d%1d%1d", &d2, &d3, &d4, &d5, &d6);
-
-    printf("Enter second group of upc digits: ");
-    scanf("%1d%1d%1d%1d%1d", &d7, &d8, &d9, &d10, &d11);
+    printf("Enter upc code: ");
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
+        &d1,
+        &d2,
+        &d3,
+        &d4,
+        &d5,
+        &d6,
+        &d7,
+        &d8,
+        &d9,
+        &d10,
+        &d11);
 
     int firstGroupSum = d1 + d3 + d5 + d7 + d9 + d11;
     int secondGroupSum = d2 + d4 + d6 + d8 + d10;
