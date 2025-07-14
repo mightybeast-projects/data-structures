66e2d69a78a5e74c7eb4c0fb91bcaa63402175a0
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-07-14 15:03:46 +0300

    -feat- : Legal date form.

--- C/src/noob-stuff/main.c
+++ C/src/noob-stuff/main.c
@@ -111,6 +111,76 @@ void brokerCommision()
     printf("Commision: %.2f\n", commision);
 }
 
+void legalDateForm()
+{
+    int day, month, year;
+
+    printf("Enter date (dd/mm/yyyy) : ");
+    scanf("%d/%d/%d", &day, &month, &year);
+
+    printf("Dated this: %d", day);
+    switch (day)
+    {
+    case 1:
+    case 21:
+    case 31:
+        printf("st ");
+        break;
+    case 2:
+    case 22:
+        printf("nd ");
+    case 3:
+    case 23:
+        printf("rd ");
+    default:
+        printf("th ");
+    }
+
+    printf("day of ");
+
+    switch (month)
+    {
+    case 1:
+        printf("January");
+        break;
+    case 2:
+        printf("February");
+        break;
+    case 3:
+        printf("March");
+        break;
+    case 4:
+        printf("April");
+        break;
+    case 5:
+        printf("May");
+        break;
+    case 6:
+        printf("June");
+        break;
+    case 7:
+        printf("July");
+        break;
+    case 8:
+        printf("August");
+        break;
+    case 9:
+        printf("September");
+        break;
+    case 10:
+        printf("October");
+        break;
+    case 11:
+        printf("November");
+        break;
+    case 12:
+        printf("December");
+        break;
+    };
+
+    printf(", %d\n", year);
+}
+
 void main()
 {
     // toCOrNotToC();
@@ -118,5 +188,6 @@ void main()
     // shippingBoxes();
     // toCelcius();
     // upcCheckDigit();
-    brokerCommision();
+    // brokerCommision();
+    legalDateForm();
 }
