209fd6f981836837cc47e92979e6598fb4b17f0a
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-07-14 14:37:23 +0300

    -feat- : Broker commision.

--- C/src/noob-stuff/main.c
+++ C/src/noob-stuff/main.c
@@ -3,9 +3,13 @@
 void toCOrNotToC()
 {
     printf("To c or not to c? That is the question.\n");
-
-    return;
 }
+
+// DO NOT USE COMMENTS
+
+/*
+THESE TOO
+*/
 
 void largestNumbers()
 {
@@ -79,18 +83,40 @@ void upcCheckDigit()
     printf("Your check digit: %d\n", dCheck);
 }
 
+void brokerCommision()
+{
+    float value;
+
+    printf("Enter trade value: ");
+    scanf("%f", &value);
+
+    float commision;
+
+    if (value < 2500)
+        commision = 30 + value * 0.017f;
+    else if (value < 6250)
+        commision = 56 + value * 0.0066f;
+    else if (value < 20000)
+        commision = 76 + value * 0.0034f;
+    else if (value < 50000)
+        commision = 100 + value * 0.0022f;
+    else if (value < 500000)
+        commision = 155 + value * 0.0011f;
+    else
+        commision = 255 + value * 0.0009f;
+
+    if (commision < 39)
+        commision = 39;
+
+    printf("Commision: %.2f\n", commision);
+}
+
 void main()
 {
     // toCOrNotToC();
     // largestNumbers();
     // shippingBoxes();
     // toCelcius();
-
-    upcCheckDigit();
+    // upcCheckDigit();
+    brokerCommision();
 }
-
-// DO NOT USE COMMENTS
-
-/*
-THESE TOO
-*/
