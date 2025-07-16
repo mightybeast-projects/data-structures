48de030e1f0623bb0dbac2c3c8e2f723f67abee0
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-07-16 16:41:16 +0300

    -feat- : Additions for types limits.

--- C/src/noob-stuff/10-types-limits.h
+++ C/src/noob-stuff/10-types-limits.h
@@ -5,26 +5,30 @@
 void typesLimits()
 {
     printf("Integer (signed | default):\n");
-    printf("Limits of an integer: [%d --> %d]\n", INT_MIN, INT_MAX);
-    printf("Limits of a short integer: [%d --> %d]\n", SHRT_MIN, SHRT_MAX);
-    printf("Limits of a long integer: [%ld --> %ld]\n", LONG_MIN, LONG_MAX);
+    printf("\tLimits of an integer: [%d --> %d]\n", INT_MIN, INT_MAX);
+    printf("\tLimits of a short integer: [%d --> %d]\n", SHRT_MIN, SHRT_MAX);
+    printf("\tLimits of a long integer: [%ld --> %ld]\n", LONG_MIN, LONG_MAX);
+    printf("\tUsing sizeof (in bites): [%lu]\n", sizeof(int));
 
     printf("Integer (unsigned):\n");
-    printf("Limits of an integer: [0 --> %u]\n", UINT_MAX);
-    printf("Limits of a short integer: [0 --> %u]\n", USHRT_MAX);
-    printf("Limits of a long integer: [0 --> %lu]\n", ULONG_MAX);
+    printf("\tLimits of an integer: [0 --> %u]\n", UINT_MAX);
+    printf("\tLimits of a short integer: [0 --> %u]\n", USHRT_MAX);
+    printf("\tLimits of a long integer: [0 --> %lu]\n", ULONG_MAX);
 
     printf("Floating types:\n");
-    printf("Limits of a float: [%e --> %e] with %d digits precision\n",
+    printf("\tLimits of a float: [%e --> %e] with %d digits precision\n",
         FLT_MIN,
         FLT_MAX,
         FLT_DIG);
-    printf("Limits of a double: [%e --> %e] with %d digits precision\n",
+    printf("\tUsing sizeof (in bites): [%lu]\n", sizeof(float));
+    printf("\tLimits of a double: [%e --> %e] with %d digits precision\n",
         DBL_MIN,
         DBL_MAX,
         DBL_DIG);
+    printf("\tUsing sizeof (in bites): [%lu]\n", sizeof(double));
 
     printf("Characters (ambiguous integer):\n");
-    printf("Limits of a signed character: [%d --> %d]\n", CHAR_MIN, CHAR_MAX);
-    printf("Limits of an unsigned character: [0 --> %d]\n", UCHAR_MAX);
+    printf("\tLimits of a signed character: [%d --> %d]\n", CHAR_MIN, CHAR_MAX);
+    printf("\tLimits of an unsigned character: [0 --> %d]\n", UCHAR_MAX);
+    printf("\tUsing sizeof (in bites): [%lu]\n", sizeof(char));
 }
